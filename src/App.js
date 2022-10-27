14c64ca4d0b0b952aa28ef9cef3212e5e5d112ac
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-10-27 14:42:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'; 
+import logo from './logo.svg';//It's importing logo
+import './App.css'; //We are importing app.css can be found the . is saying it is in the same folder #app.css
 
+  const names = ['Gustavo', 'Gerly', 'Cassandra', 'Corwin', 
+'Jacques', 'Ainis', 'Gianna', 'Emily', 'Fanessa', 'Adi', 
+'Mido', 'Carl', 'Eddie', 'Captain Ed', 'Nick', 'Tyler',
+'Todd', 'Brad']
+  
 function App() {
+
+  const [myName, setMyName] =useState ('Todd')
+  const [counter, setCounter] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {myName} is pretty dam cool. 
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {names.map(name => (
+          <button key={name} onClick={() => setMyName(name)}>{name}</button>
+
+        ))}
+        <button onClick={() => setMyName ('Meghann')}>Meghan</button>
+        <button onClick={() => setMyName ('Noah')}>Noah</button>
+        <button onClick={() => setMyName ('Gerly')}>Gerly</button>
+        <p>You clicked the button {counter} times</p>
+        <button onClick={() => setCounter((counter > 0) ? counter -1 : 0)}>-1</button>
+        <button onClick={() => setCounter(0)}>Reset</button>
+        <button onClick={() => setCounter(counter +1)}>+1</button>
       </header>
     </div>
   );
